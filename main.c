17dde8c7d61a82531d524cb1daa6c5ccded7cdd8
George Maratos <ffmpeg12341@gmail.com>
2015-06-08 20:14:48 -0500

    have an input reader up to cone coordinate reading, will test now

--- main.c
+++ main.c
@@ -12,16 +12,44 @@ struct cone_coord {
 int test_cases = 0, radius_inner = 0, radius_outer = 0;
 int cone_number = 0; 
 
-void input_reader(void) {
+int input_reader(FILE *f) {
   char buf[10], *tok = NULL;
-  fgets(buf,10,stdin);
-  tok = strtok(buf," ");
+  fgets(buf,10,f);
+  tok = strtok(buf," \n");
   if(!tok) {
     printf("Error: no test cases\n");
-    return ;
+    return 1;
   }
   test_cases = atoi(tok);
+  fgets(buf,10,f);
+  tok = strtok(buf," ");
+  if(!tok) {
+    printf("Error: no boundary defined\n");
+    return 1;
+  }
+  radius_inner = atoi(tok);
+  tok = strtok(NULL," \n");
+  if(!tok) {
+    printf("Error: no boundary defined2\n");
+    return 1;
+  }
+  radius_outer = atoi(tok);
+  fgets(buf,10,f);
+  tok = strtok(buf," \n");
+  if(!tok) {
+    printf("Error: no cone # defined\n");
+    return 1;
+  }
+  cone_number = atoi(tok);
+  return 0;
 }
 
-void main(void) {
+int main(int argc, char **argv) {
+  if(argc != 2) {
+    printf("Error: improper number of arguements\n");
+    return 1;
+  }
+  FILE *f = NULL;
+  f = fopen(argv[1],"r");
+  return input_reader(f);
 }
